8db1fafeb7163d12cc05059e8d6e5e1374373ce7
u20181a686 (Tiglla Arrascue, Bruno) <u20181a686@upc.edu.pe>
2019-11-14 13:54:09 -0500

    cambios de Renzo :V //

--- TF/TF/Main.cpp
+++ TF/TF/Main.cpp
@@ -1,3 +1,4 @@
+#include "AVL.h"
 #include "Producto.h"
 #include <vector>
 #include <sstream>
@@ -6,6 +7,7 @@
 
 using namespace System;
 namespace fs = experimental::filesystem;
+
 
 void menu()
 {
@@ -107,22 +109,51 @@ bool SOS()
 	}
 }
 
+
+void index_c(vector<string> Nombre, vector<int> Eleccion)
+{
+	AVLTree<int> elec;
+	for (int i = 0; i < Eleccion.size(); i++)
+	{
+		cout << Nombre[i] << "\t" << Eleccion[i] << endl;
+		elec.Add(Eleccion[i]);
+	}
+}
+
+int empiezacon(vector<string> Nombre, char a)
+{
+	for (int i = 0; i < Nombre.size(); i++)
+	{
+		string compare = Nombre[i];
+		if (compare[0] == a)
+		{
+			return i;
+		}
+	}
+}
+
+
 void main() {
 
 	Console::SetWindowSize(115, 28);
-	if (SOS())
+	if (1)
 	{
 		system("cls");
 		menu();
 		system("pause>0");
 		system("cls");
 		vector<fs::directory_entry> vec;
+		vector<string> filename;
+		vector<string> extension;
+		vector<int> size;
 		vector<string> _nombre;
 		vector<int> _codigo;
 		vector<int> _stock;
 		vector<int> _produccion;
 		vector<int> _vencimiento;
-		fs::path RUTA{ "archivos" };
+		string _archivo;
+		cout << "	Ingrese el nombre del archivo que quiere explorar: "; cin >> _archivo; cout << endl;
+		fs::path RUTA{ _archivo };
 		for (auto& p : fs::recursive_directory_iterator(RUTA))
 		{
 			vec.push_back(p);
@@ -141,7 +172,12 @@ void main() {
 				}
 				Name += aPath.filename().string()[i];
 			}
+
+			filename.push_back(Name);
+			extension.push_back(aPath.extension().string());
+			size.push_back(fs::file_size(vec[i]));
 			cout << Name << "\t" << aPath.extension() << "\t\t" << fs::file_size(vec[i]) << "B" << endl;
+
 		}
 
 		for (int i = 0; i < vec.size(); i++)
@@ -179,13 +215,40 @@ void main() {
 					_vencimiento.push_back(obj_producto->get_Vencimiento());
 				}
 			}
+			f.close();
 		}
 		cout << "Nombre\t\tCodigo\tStock\tProduccion\tVencimiento" << endl;
 		for (int i = 0; i < vec.size(); i++)
 		{
-			cout << _nombre[i] << "\t\t" << _codigo[i] << "\t" << _stock[i] << "\t" << _produccion[i] << "\t" << _vencimiento[i] << endl;;
+			cout << _nombre[i] << "\t\t" << _codigo[i] << "\t" << _stock[i] << "\t" << _produccion[i] << "\t" << _vencimiento[i] << endl;
 		}
-	}	
+		system("pause>0");
+		system("cls");
 
-	system("pause>0");;
+		string _eleccion;
+		cout << "Ingrese el nombre de la caracteristica para indexar : "; cin >> _eleccion;
+		if (_eleccion == "Codigo")
+		{
+			index_c(_nombre, _codigo);		
+		}
+		if (_eleccion == "Stock")
+		{
+			index_c(_nombre, _stock);
+		}
+		if (_eleccion == "Produccion")
+		{
+			index_c(_nombre, _produccion);
+		}
+		if (_eleccion == "Vencimiento")
+		{
+			index_c(_nombre, _vencimiento);
+		}
+		cout << "ingrese tecla :";
+		char t;
+		cin >> t;
+		cout << filename[empiezacon(filename, t)] << "\t" << extension[empiezacon(filename, t)] << "\t" << size[empiezacon(filename, t)] << endl;
+	
+	}	
+	cin.ignore();
+	system("pause>0");
 }
